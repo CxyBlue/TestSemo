45ba1ef521a41853763b9f9868f524099da34aaa
CxyBlue <1940579695@qq.com>
2018-03-02 10:13:51 +0800

    测试

---
+++ testDemo/testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by 086 on 2018/3/2.
+//  Copyright © 2018年 MCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
